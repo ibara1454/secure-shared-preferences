3027294559610b3e57db24378c6202a3c4ff0a3b
Ibara <ibara1454@gmail.com>
2020-05-09 19:29:20 +0900

    docs: add doc to Encrypter

--- secure-shared-preferences/src/main/java/com/github/ibara1454/secure_shared_preferences/cipher/Encrypter.kt
+++ secure-shared-preferences/src/main/java/com/github/ibara1454/secure_shared_preferences/cipher/Encrypter.kt
@@ -1,5 +1,13 @@
 package com.github.ibara1454.secure_shared_preferences.cipher
 
+/**
+ * The encrypter provides the method [encrypt] to encrypt any plain text.
+ */
 interface Encrypter<T> {
+    /**
+     * Encrypt the given text.
+     * @param text any text.
+     * @returns encrypted text.
+     */
     fun encrypt(text: T): T
 }
