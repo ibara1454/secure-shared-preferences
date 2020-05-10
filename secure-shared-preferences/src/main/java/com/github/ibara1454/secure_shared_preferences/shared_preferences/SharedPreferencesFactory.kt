c5f3e15675b846e48ae907f47aa61da1699a51bb
Ibara <ibara1454@gmail.com>
2020-05-10 23:15:49 +0900

    docs: add docs to SharedPreferencesFactory

--- secure-shared-preferences/src/main/java/com/github/ibara1454/secure_shared_preferences/shared_preferences/SharedPreferencesFactory.kt
+++ secure-shared-preferences/src/main/java/com/github/ibara1454/secure_shared_preferences/shared_preferences/SharedPreferencesFactory.kt
@@ -2,6 +2,17 @@ package com.github.ibara1454.secure_shared_preferences.shared_preferences
 
 import android.content.SharedPreferences
 
+/**
+ * The standard interface defines a [create] method to return [SharedPreferences].
+ */
 interface SharedPreferencesFactory {
+    /**
+     * Create a [SharedPreferences].
+     *
+     * @param name name of preferences.
+     * @param mode operating mode. This parameter is same as the mode parameter in normal
+     *  [SharedPreferences].
+     * @return [SharedPreferences].
+     */
     fun create(name: String, mode: Int): SharedPreferences
 }
