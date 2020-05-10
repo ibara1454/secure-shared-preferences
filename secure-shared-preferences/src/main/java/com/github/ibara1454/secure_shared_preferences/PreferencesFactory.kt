ede50492f0f0617a90bc3010abbbb120ba55960c
Ibara <ibara1454@gmail.com>
2020-05-10 19:49:13 +0900

    refactor: remove unused class

--- secure-shared-preferences/src/main/java/com/github/ibara1454/secure_shared_preferences/PreferencesFactory.kt
+++
@@ -1,7 +0,0 @@
-package com.github.ibara1454.secure_shared_preferences
-
-import android.content.SharedPreferences
-
-interface PreferencesFactory {
-    fun create(name: String, mode: Int): SharedPreferences
-}
