6b2f10a44465a5fc041009246dcdace562a247e6
Ibara <ibara1454@gmail.com>
2020-05-10 18:18:06 +0900

    docs: add docs to StringDecrypter / StringEncrypter

--- secure-shared-preferences/src/main/java/com/github/ibara1454/secure_shared_preferences/cipher/StringDecrypter.kt
+++ secure-shared-preferences/src/main/java/com/github/ibara1454/secure_shared_preferences/cipher/StringDecrypter.kt
@@ -1,15 +1,32 @@
 package com.github.ibara1454.secure_shared_preferences.cipher
 
+/**
+ * The decrypter is a adapter which accept a `byte-array-to-byte-array` decrypter and convert it
+ * to a `string-to-string` decrypter.
+ */
 internal class StringDecrypter(private val decrypter: Decrypter<ByteArray>) : Decrypter<String> {
 
+    /**
+     * Constructor for converting given function to [StringDecrypter] (since Kotlin 1.3 doesn't
+     * support SAM conversion on Kotlin).
+     * @param decrypt any decryption function which transforms encrypted data to plain data.
+     */
     constructor(decrypt: (ByteArray) -> ByteArray) : this(object : Decrypter<ByteArray> {
         override fun decrypt(text: ByteArray): ByteArray = decrypt(text)
     })
 
     private val charset = Charsets.UTF_8
 
+    /**
+     * Convert the given encrypted [text] to original text.
+     * @param text any encrypted string.
+     * @return original text.
+     */
     override fun decrypt(text: String): String {
         val input = text.toByteArray(charset)
+        // `toString` will not throw any exception and always replaces malformed-input and
+        //  unmappable-character sequences with this charset's default replacement string.
+        // But `decrypter::decrypt` may throw exceptions. It depends on the given wrapped decrypter.
         return decrypter.decrypt(input).toString(charset)
     }
 }
--- secure-shared-preferences/src/main/java/com/github/ibara1454/secure_shared_preferences/cipher/StringEncrypter.kt
+++ secure-shared-preferences/src/main/java/com/github/ibara1454/secure_shared_preferences/cipher/StringEncrypter.kt
@@ -1,15 +1,32 @@
 package com.github.ibara1454.secure_shared_preferences.cipher
 
+/**
+ * The encrypter is a adapter which accept a `byte-array-to-byte-array` encrypter and convert it
+ * to a `string-to-string` encrypter.
+ */
 internal class StringEncrypter(private val encrypter: Encrypter<ByteArray>) : Encrypter<String> {
 
+    /**
+     * Constructor for converting given function to [StringEncrypter] (since Kotlin 1.3 doesn't
+     * support SAM conversion on Kotlin).
+     * @param encrypt any encryption function which transforms plain data to encrypted data.
+     */
     constructor(encrypt: (ByteArray) -> ByteArray) : this(object : Encrypter<ByteArray> {
         override fun encrypt(text: ByteArray): ByteArray = encrypt(text)
     })
 
     private val charset = Charsets.UTF_8
 
+    /**
+     * Convert the given [text] to encrypted text.
+     * @param text any string.
+     * @return encrypted text.
+     */
     override fun encrypt(text: String): String {
         val input = text.toByteArray(charset)
+        // `toString` will not throw any exception and always replaces malformed-input and
+        //  unmappable-character sequences with this charset's default replacement string.
+        // But `encrypter::encrypt` may throw exceptions. It depends on the given wrapped encrypter.
         return encrypter.encrypt(input).toString(charset)
     }
 }
